d5df5da000ca5688d71205abc3c608bd615e7a3e
Lakshay <dmslakshay@gmail.com>
2024-02-16 19:45:00 +0530

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const oracle = require('oracledb');
+const app = express();
+
+app.listen(5000, () => {console.log('listening on 3000')});
+
+app.get('/login', (req, res) => {
+    async function getData(){
+        try{
+            let connection = await oracle.getConnection({
+                user: 'HR',
+                password: '123',
+                connectString: 'localhost/xe'
+            });
+            const res = await connection.execute('SELECT * FROM HR.signup');
+        }catch(err){
+            console.error(err);
+        }   
+    }
+    getData().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        res.send(err);
+    });
+});
