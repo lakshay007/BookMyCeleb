55966d807189fdabea164b715bc2750d0886b88a
Lakshay <dmslakshay@gmail.com>
2024-02-25 17:31:44 +0530

    backend stuff

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ app.listen(5000, () => {console.log('listening on 3000')});
 app.get('/', (req, res) => {
     res.send('Hello World');
 }   );
-app.get('/login', (req, res) => {
+let connection = await oracle.getConnection({
+    user: 'HR',
+    password: '123',
+    connectString: 'localhost/xe'
+});
+app.get('/UserData/:username', (req, res) => {
     async function getData(){
         try{
-            let connection = await oracle.getConnection({
-                user: 'HR',
-                password: '123',
-                connectString: 'localhost/xe'
-            });
-            const res = await connection.execute('SELECT * FROM HR.signup');
+            const res = await connection.execute('SELECT * FROM HR.signup WHERE USERNAME = :username', [req.params.username], {outFormat: oracle.OUT_FORMAT_OBJECT});
             return res.rows;
         }catch(err){
             console.error(err);
